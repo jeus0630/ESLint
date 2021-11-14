aac455eb50c2a334c1b91b01e2b3b0a5eb9d932a
jewoo <jeus0630@gmail.com>
2021-11-14 11:55:43 -0500

    ESLint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+  },
+};
